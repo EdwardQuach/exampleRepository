4f072f4921900ddf697a2fb5e8cabad236d93184
Edward <vkspecialed@gmail.com>
2022-03-23 13:36:24 -0700

    ninth solution

--- problems/09-chain-map.js
+++ problems/09-chain-map.js
@@ -29,13 +29,34 @@ console.log(chainMap(4, square, half));         // 8
 console.log(chainMap(4, half, square));         // 4
 *******************************************************************************/
 
-let chainMap = function() {
-
+let chainMap = function(num, ...cb) {
+    let result = num;
+    for (let i = 0; i < cb.length; i++){
+        let ele = cb[i];
+        result = ele(result);
+        
+    }
+    return result;
 };
 
 
+let add5 = function(n) {
+    return n + 5;
+};
 
+let half = function(n) {
+    return n / 2;
+};
 
+let square = function(n) {
+    return n * n;
+};
+
+console.log(chainMap(25, add5));                // 30
+console.log(chainMap(25, add5, half));          // 15
+console.log(chainMap(25, add5, half, square));  // 225
+console.log(chainMap(4, square, half));         // 8
+console.log(chainMap(4, half, square));         // 4
 
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
